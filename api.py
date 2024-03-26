800c9413dea61e773bba19d1662621ee91624b91
suraj-p-nair <98915139+albintshibu@users.noreply.github.com>
2024-03-26 22:49:52 +0530

    system test

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from googleapiclient.discovery import build 
+from google.auth.exceptions import RefreshError
 from google_auth_oauthlib.flow import InstalledAppFlow 
 from google.auth.transport.requests import Request 
 import pickle 
@@ -24,51 +25,49 @@ def extract_emails(text):
 
     return "\n".join(matches)
 
-def getEmails(): 
-	creds = None
-	if os.path.exists('token.pickle'): 
-		with open('token.pickle', 'rb') as token: 
-			creds = pickle.load(token)
+def getEmails():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
 
-	if not creds or not creds.valid: 
-		if creds and creds.expired and creds.refresh_token: 
-			creds.refresh(Request()) 
-		else: 
-			flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES) 
-			creds = flow.run_local_server(port=0) 
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            try:
+                creds.refresh(Request())
+            except RefreshError:
+                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+                with open('token.pickle', 'wb') as token:
+                    pickle.dump(creds, token)
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
 
-		with open('token.pickle', 'wb') as token: 
-			pickle.dump(creds, token) 
-		
+    service = build('gmail', 'v1', credentials=creds)
 
-	service = build('gmail', 'v1', credentials=creds) 
-
-	result = service.users().messages().list(userId='me').execute() 
-
-
-	messages = result.get('messages') 
+    result = service.users().messages().list(userId='me').execute()
+    messages = result.get('messages') 
 
 	
-	for msg in messages: 
-		txt = service.users().messages().get(userId='me', id=msg['id']).execute() 
-
-		ans = ""
-		ans += "BODY:	" + (txt['snippet'] + "\n")
-
-		txt = txt['payload']['headers']
-		
-		for i in txt:
-			if i['name'] == "To":
-				ans += "TO:		"+(str(i['value']) + "\n")
-			if i['name'] == "Subject":
-				ans += "SUBJECT:	" + (str(i['value']) + "\n")
-			if i['name'] == "From":
-				ans += "FROM:	" + (str(i['value']) + "\n")
-		f.write(str(ans))
-
-		print(extract_urls(ans))
-		print(extract_emails(ans))
-		break
+    for msg in messages: 
+        txt = service.users().messages().get(userId='me', id=msg['id']).execute() 
+        ans = ""
+        ans += "BODY:	" + (txt['snippet'] + "\n")
+        txt = txt['payload']['headers']
+        for i in txt:
+            if i['name'] == "To":
+                ans += "TO:		"+(str(i['value']) + "\n")
+            if i['name'] == "Subject":
+                ans += "SUBJECT:	" + (str(i['value']) + "\n")
+            if i['name'] == "From":
+                ans += "FROM:	" + (str(i['value']) + "\n")
+        f.write(str(ans))
+        print(extract_urls(ans))
+        print(extract_emails(ans))
+        break
 
 
 getEmails()
